cea36f5fcb113f7d6d8e450b1c3d6b675f5bc048
enzoovechkin-source <enzoovechkin@gmail.com>
2025-10-09 20:19:12 +0200

    Restore PR29 application state

--- src/App.tsx
+++ src/App.tsx
@@ -22,12 +22,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter
-          future={{
-            v7_startTransition: true,
-            v7_relativeSplatPath: true,
-          }}
-        >
+        <BrowserRouter>
           <Routes>
             <Route path="/auth" element={<Auth />} />
             <Route path="/" element={
