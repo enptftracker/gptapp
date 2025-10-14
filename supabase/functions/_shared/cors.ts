15748ede790c0e2543f15390117f0365ada2b564
enzoovechkin-source <enzoovechkin@gmail.com>
2025-10-14 12:27:08 +0200

    Update cors.ts

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,5 @@
 const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://gptapp-khaki.vercel.app',
+  'Access-Control-Allow-Origin': 'https://gptapp-khaki.vercel.app','https://majfmrisrwhdsmrvyfzm.supabase.co/functions/v1/fetch-stock-price',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 };
 
