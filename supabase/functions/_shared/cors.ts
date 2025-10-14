b174f7d10f9410a01ea74b171671da63e2063380
enzoovechkin-source <enzoovechkin@gmail.com>
2025-10-14 12:24:36 +0200

    Update cors.ts

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,5 @@
 const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Origin': 'https://gptapp-khaki.vercel.app',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 };
 
